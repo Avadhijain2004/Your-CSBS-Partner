dff80caad90ac073e98e006e2cf489823f561bc4
Avadhi <avadhijain2004@gmail.com>
2024-09-08 21:36:57 +0530

    Problem resolved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import HeroContent from './HeroContent'
 import Services from './Services'
 import About from './About'
 import Contact from './Contact'
-import FileDownloadButton from './FileDownloadButton'
+
 
 function Home() {
     return (
